6bb158a7bb57c1e5c327d0e31a8d0685adf066a0
Verma <aman.n.verma@accenture.com>
2022-07-21 10:53:47 +0530

    File Picker changes

--- src/components/PlanboardDesigner/component/Canvas/components/IdeationFlow/Filepicker.js
+++ src/components/PlanboardDesigner/component/Canvas/components/IdeationFlow/Filepicker.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { Typography } from '@mui/material';
 const Filepicker = () => {
 	const [files, setFiles] = useState([]);
 	const onDrop = useCallback((accFiles, rejFiles) => {
@@ -10,9 +11,23 @@ const Filepicker = () => {
 	const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
 	return (
-		<div {...getRootProps()}>
+		<div {...getRootProps()} style={{background:'lightgrey',position:'relative',margin:4,border:'2px solid grey',cursor:'pointer'}}>
 			<input {...getInputProps()} />
-			<p>Drag & drop some files here, or click to select files</p>
+			<div>
+				<UploadFileIcon
+					style={{
+						fontSize: 65,
+						color: 'blueviolet',
+						width: '95%',
+						margin: 'auto',
+					}}
+				/>
+				<div style={{ width: 350, margin: 'auto' }}>
+					<Typography variant='h6' gutterBottom component='div'>
+						Click or drag file to this area to upload
+					</Typography>
+				</div>
+			</div>
 			{JSON.stringify(files)}
 		</div>
 	);
